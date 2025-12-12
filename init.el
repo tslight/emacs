9dc887baa49ccf9255dfc004f919d3bf6139ebc0
tslight <tslight@pm.com>
2025-12-12 09:54:26 +0000

    better defaults

--- init.el
+++ init.el
@@ -2,19 +2,25 @@
 (use-package emacs
   :config
   (load-theme 'modus-vivendi)
+  (set-frame-font "Monospace 11" nil t)
   (menu-bar-mode -1)
   (scroll-bar-mode -1)
   (tool-bar-mode -1)
   (tooltip-mode -1)
+  (display-time)
+  (display-battery-mode)
   (defun my/indent-buffer ()
     "Indent the contents of a buffer."
     (interactive)
     (indent-region (point-min) (point-max)))
   :custom
   (custom-file (expand-file-name "custom.el" user-emacs-directory))
+  (display-time-format "%H:%M %a %d/%m")
+  (display-time-default-load-average 'nil)
   (inhibit-startup-screen t)
   (use-short-answers t)
   (auto-save-visited-mode t)
+  (disabled-command-function nil)
   (completion-styles '(initials flex partial-completion))
   (package-archives
    '(("gnu" . "https://elpa.gnu.org/packages/")
